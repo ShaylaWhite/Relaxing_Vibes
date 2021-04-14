08d5f558898afe9e8975bd6bed290117e96241a4
Shayla <shayladwhite1@gmail.com>
2021-04-13 19:37:21 -0700

    Single Block if Statements

--- masterjs.js
+++ masterjs.js
@@ -17,21 +17,15 @@ let studentAverage = 40;
 let passed = 70;
 
 
-function passedExam() {
-  if(){
-
+function passedExam(studentAverage, passed) {
+  if(studentAverage > passed){
+    console.log('You Passed')
   }else{
-
-
+    console.log('You Failed')
   }
-
-
 }
-
-
-
-
-
 
 passedExam(40,50);
 passedExam(40,77);
+
+// Single if Blocks
