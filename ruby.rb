14c82d4607d0c95d6d104b9ea792d06da2e43d6b
Shayla <shayladwhite1@gmail.com>
2021-04-28 19:54:13 -0700

    writing out steps to solve problem

--- ruby.rb
+++ ruby.rb
@@ -1,3 +1,8 @@
 ruby.rb
-/Given an array of numbers, we want to count how many items are greater than 5/
-/ Array=[17,6,3,7,9,1]
+#Given an array of numbers, we want to count how many items are greater than 5/
+#Array=[17,6,3,7,9,1]
+
+#Go through all numbers in the Array
+#For each number compare to 5
+#if number is higher than 5 add to inetger if not do not count
+#
